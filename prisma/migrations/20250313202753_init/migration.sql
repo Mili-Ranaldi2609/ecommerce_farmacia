27e039ec688fbcec087e4ca58720e37c74db0c83
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2025-03-13 17:38:37 -0300

    fixes-dbMigration

--- prisma/migrations/20250313202753_init/migration.sql
+++ prisma/migrations/20250313202753_init/migration.sql
@@ -21,6 +21,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "googleBool" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
